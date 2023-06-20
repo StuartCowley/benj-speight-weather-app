d46f4a8a63fb61361fdd41cce73935ec2eb5acf9
Benj <ben.speight@outlook.com>
2023-06-20 21:15:59 +0100

    refactor

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,18 +1,22 @@
-import React from "react";
+import iconData from "../data/iconData.json";
 
 function ForecastSummary(props) {
-  const { date, description, icon, temperature } = props;
+  const { date, description, icon, onSelect, temperature } = props;
+  const formattedDate = new Date(date).toDateString();
+
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
-      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__date">{formattedDate}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        {icon}
+        <img src={iconData[icon]} alt="icon" />
       </div>
       <div className="forecast-summary__temperature">
-        {temperature.max}
-        &deg;C
+        {temperature.max} &deg;C
       </div>
       <div className="forecast-summary__description">{description}</div>
+      <button type="button" onClick={() => onSelect(date)}>
+        More details:
+      </button>
     </div>
   );
 }
--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,16 +1,16 @@
-import React from "react";
 import { render } from "@testing-library/react";
-import ForecastSummary from "../../Components/ForecastSummary";
+import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
   const validProps = {
-    date: 1111111,
+    date: "Thu Jan 01 1970",
     description: "Stub description",
-    icon: "stubIcon",
+    icon: "800",
     temperature: {
       min: 12,
       max: 22,
     },
+    onSelect: () => {},
   };
 
   it("renders correctly", () => {
@@ -20,12 +20,14 @@ describe("ForecastSummary", () => {
         description={validProps.description}
         icon={validProps.icon}
         temperature={validProps.temperature}
+        onSelect={validProps.onSelect}
       />
     );
+
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it("render correct values for props", () => {
+  it("renders correct values for props", () => {
     const { getByText, getByTestId } = render(
       <ForecastSummary
         date={validProps.date}
@@ -34,16 +36,12 @@ describe("ForecastSummary", () => {
         temperature={validProps.temperature}
       />
     );
-    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
-    expect(getByText("Stub description")).toHaveClass(
-      "forecast-summary__description"
-    );
 
-    expect(getByText("1111111")).toHaveClass("forecast-summary__date");
+    expect(getByText("Thu Jan 01 1970")).toHaveClass("forecast-summary__date");
     expect(getByText("Stub description")).toHaveClass(
       "forecast-summary__description"
     );
     expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary__icon");
-    expect(getByText("22°C")).toHaveClass("forecast-summary__temperature");
+    expect(getByText("22 °C")).toHaveClass("forecast-summary__temperature");
   });
 });
